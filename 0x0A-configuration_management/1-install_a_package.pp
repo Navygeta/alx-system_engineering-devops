360330a08095cafa95acf23a2c06c1c0a6cd26b2
Navygeta <tinashenavonika@gmail.com>
2024-01-20 01:34:41 +0200

    install pckg

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,10 @@
 #!/usr/bin/pup
-# Install a specific version of flask (2.1.0)
-
+# Install flask (2.1.0)
 package { 'python3-pip':
   ensure => installed,
 }
-
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pipi3',
 }
+

3011e284aab1597209824752080c2b07d7a34ae8
Navygeta <tinashenavonika@gmail.com>
2024-01-20 01:29:14 +0200

    install package flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #!/usr/bin/pup
 # Install a specific version of flask (2.1.0)
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pipi3',
 }

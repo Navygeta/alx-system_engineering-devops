2aff6f9772ad410e5ac8fb8f5681879e20e62f23
Navygeta <tinashenavonika@gmail.com>
2024-01-20 01:39:43 +0200

    install package corrected

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,18 @@
 #!/usr/bin/pup
 # Install flask (2.1.0)
-package { 'python3-pip':
+
+package { 'python3-pip3':
   ensure => installed,
 }
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pipi3',
+}
+
+package { 'werkzeug':
+  ensure => '2.0.2',
+  provider => 'pip3',
+  require => Package['python3-pip3']
 }
 

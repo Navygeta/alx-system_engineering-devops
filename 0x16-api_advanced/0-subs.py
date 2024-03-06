e5f90f29094f7874baa7ed00eca3ef4cad0a907f
Navygeta <tinashenavonika@gmail.com>
2024-03-06 13:04:00 +0200

    correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,20 +9,17 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Returns the total number of subscribers for a given subreddit.
-
-    :param subreddit: The name of the subreddit.
-    :return: The total number of subscribers or 0 if the subreddit is invalid.
+    Retrieves the total number of subscribers for a specified subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "CustomUserAgent"}
+    api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    request_headers = {"User-Agent": "Mozilla/5.0"}
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(api_url, headers=request_headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            subscribers = data.get('data', {}).get('subscribers', 0)
-            return subscribers
+            subscribers_count = data['data']['subscribers']
+            return subscribers_count
     except requests.RequestException as e:
         print(f"Error: {e}")
 

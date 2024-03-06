312bd6cd2680aa9f78b2c9ea4e41862b95b86723
Navygeta <tinashenavonika@gmail.com>
2024-03-06 13:51:31 +0200

    top ten 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
+"""
+Script to print hot posts on a given Reddit subreddit.
+"""
 
 import requests
 
 
-def get_hot_posts(subreddit, limit=10):
+def top_ten(subreddit):
+    """
+    Print the titles of the 10 hottest posts on a given subreddit.
+
+    :param subreddit: The name of the subreddit.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    params = {"limit": limit}
+    params = {"limit": 10}
 
-    try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
-        response.raise_for_status()
-        results = response.json().get("data")
-        return [c.get("data").get("title") for c in results.get("children")]
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
-    except requests.RequestException as e:
-        raise Exception(f"Error making request to Reddit API: {e}")
-
-
-def print_top_ten_titles(subreddit):
-    hot_posts = get_hot_posts(subreddit, limit=10)
-
-    if not hot_posts:
+    if response.status_code == 404:
         print("None")
         return
 
-    for index, title in enumerate(hot_posts, start=1):
-        print(f"{index}. {title}")
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
 
 
 if __name__ == "__main__":
     subreddit_name = "python"
-    print_top_ten_titles(subreddit_name)
+    top_ten(subreddit_name)

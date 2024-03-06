b9cf1da8f8e7700185d326374a54a4932fc2fdc2
Navygeta <tinashenavonika@gmail.com>
2024-03-06 13:58:17 +0200

    recurse it

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def recurse(subreddit, hot_list=None, after="", count=0):
+    if hot_list is None:
+        hot_list = []
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    params = {"after": after, "count": count, "limit": 100}
+
+    response = requests.get(
+        url, headers=headers, params=params, allow_redirects=False
+    )
+
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+
+    return hot_list
